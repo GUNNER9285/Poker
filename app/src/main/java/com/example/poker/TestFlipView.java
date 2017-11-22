dede9ee766f6834b6a2e10c730c70282136fdbd6
GUNNER9285 <gunner9285@gmail.com>
2017-11-22 19:40:43 +0700

    add library card flip fix picture

--- app/src/main/java/com/example/poker/TestFlipView.java
+++ app/src/main/java/com/example/poker/TestFlipView.java
@@ -3,7 +3,7 @@ package com.example.poker;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class TestFlipView extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
